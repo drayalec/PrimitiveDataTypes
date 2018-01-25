7883fb5620c040a862335199724c545a468e83c2
drayalec <31783033+drayalec@users.noreply.github.com>
2018-01-24 23:40:44 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,27 @@
+// This program will create an variable of each of the 6 primitive data types. It will print the variables and
+// indicate what data type each one is.
+
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte myByte = 5;
+        short myShort = 10;
+        int myInt = 75;
+        long myLong = 566;
+        float myFloat = 2.16f;
+        boolean myBoolean = true;
+        char myChar = 't';
+
+        System.out.print("Byte Demo: " + myByte + '\n');
+        System.out.print("Short Demo: " + myShort + '\n');
+        System.out.print("Integer Demo: " + myInt + '\n');
+        System.out.print("Long Demo: " + myLong + '\n');
+        System.out.print("Float Demo: " + myFloat + '\n');
+        System.out.print("Boolean Demo: " + myBoolean + '\n');
+        System.out.print("Character Demo: " + myChar + '\n');
+
+    }
+}
